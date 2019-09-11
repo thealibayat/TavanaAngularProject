e2e50a5396d2b5d96995944bd39f8c775232bcb6
alibayat <bayat@mail.com>
2019-09-11 14:06:12 +0430

    Fix conflict2

--- src/app/shared-main/main.module.ts
+++ src/app/shared-main/main.module.ts
@@ -2,25 +2,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-<<<<<<< HEAD
-
-
 @NgModule({
-  imports:      [ CommonModule ],
-  declarations: [ ],
-  exports:      [CommonModule, FormsModule ]
-=======
-@NgModule({
-  imports:      [ 
-    CommonModule 
+  imports: [
+    CommonModule
   ],
   declarations: [
 
-  ],
-  exports:[
-     CommonModule,
-      FormsModule 
-    ]
->>>>>>> 9c2b5c9e20a17178c4485a9056e7218094733581
+    ],
+  exports: [
+    CommonModule,
+    FormsModule
+  ]
 })
 export class MainModule { }
