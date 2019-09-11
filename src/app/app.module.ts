147e168071c28ac1504be87954354c9bd19344a8
alibayat <bayat@mail.com>
2019-09-11 10:28:26 +0430

    Predected components initialized.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './home-module/comp/nav-bar/nav-bar.component';
+import { FooterComponent } from './home-module/comp/footer/footer.component';
+import { BodyComponent } from './home-module/comp/body/body.component';
+import { HeaderComponent } from './home-module/comp/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    BodyComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
