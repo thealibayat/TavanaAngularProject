472ea74479238ccd9c970b148f924f9e46507251
alibayat <bayat@mail.com>
2019-09-11 13:50:37 +0430

    Navbar
    navbar preimplemented.

--- src/app/shared-main/main.module.ts
+++ src/app/shared-main/main.module.ts
@@ -2,14 +2,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { CustomerComponent } from './customer.component';
-import { NewItemDirective } from './new-item.directive';
-import { OrdersPipe } from './orders.pipe';
+
 
 @NgModule({
   imports:      [ CommonModule ],
-  declarations: [ CustomerComponent, NewItemDirective, OrdersPipe ],
-  exports:      [ CustomerComponent, NewItemDirective, OrdersPipe,
-    CommonModule, FormsModule ]
+  declarations: [ ],
+  exports:      [CommonModule, FormsModule ]
 })
 export class MainModule { }
